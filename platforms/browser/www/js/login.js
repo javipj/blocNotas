b6cdd92dd4768b6ebb733bdd3503353f4987816c
javi <javipj@gmail.com>
2017-05-09 17:36:34 +0200

    test

--- platforms/browser/www/js/login.js
+++ platforms/browser/www/js/login.js
@@ -26,6 +26,22 @@ function iniciarFireBase(){
   };
   firebase.initializeApp(config);
 
+	firebase.auth().getRedirectResult().then(function(result) {
+	  if (result.credential) {
+	    // This gives you a Google Access Token.
+	    // You can use it to access the Google API.
+	    var token = result.credential.accessToken;
+	    // The signed-in user info.
+	    var user = result.user;
+		alert("logado");
+	  }
+	}).catch(function(error) {
+	  // Handle Errors here.
+	  var errorCode = error.code;
+	  var errorMessage = error.message;
+		alert(error.message);
+	});
+
 }
 
 
