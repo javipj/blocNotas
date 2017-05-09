25821033ca415fbccfdb984a73335dd4549d0eef
javi <javipj@gmail.com>
2017-05-09 17:56:30 +0200

    test

--- platforms/browser/www/js/login.js
+++ platforms/browser/www/js/login.js
@@ -26,21 +26,7 @@ function iniciarFireBase(){
   };
   firebase.initializeApp(config);
 
-	firebase.auth().getRedirectResult().then(function(result) {
-	  if (result.credential) {
-	    // This gives you a Google Access Token.
-	    // You can use it to access the Google API.
-	    var token = result.credential.accessToken;
-	    // The signed-in user info.
-	    var user = result.user;
-		alert("logado");
-	  }
-	}).catch(function(error) {
-	  // Handle Errors here.
-	  var errorCode = error.code;
-	  var errorMessage = error.message;
-		alert(error.message);
-	});
+	
 
 }
 
@@ -90,14 +76,23 @@ function lFacebook(){
 
 function checkloginInicio(){
 alert("inicio checklogin");
-	firebase.auth().onAuthStateChanged(function(user) {
-	  if (user) {
-		  window.location.replace("listarNotas.html");
-	  } else {
-	    // No user is signed in.
-		alert("nousersigin");
+
+firebase.auth().getRedirectResult().then(function(result) {
+	  if (result.credential) {
+	    // This gives you a Google Access Token.
+	    // You can use it to access the Google API.
+	    var token = result.credential.accessToken;
+	    // The signed-in user info.
+	    var user = result.user;
+		alert("logado");
 	  }
+	}).catch(function(error) {
+	  // Handle Errors here.
+	  var errorCode = error.code;
+	  var errorMessage = error.message;
+		alert(error.message);
 	});
+
 
 }
 
