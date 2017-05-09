5f37cc1f044e9a5cfee768772e827edd863639e9
javi <javipj@gmail.com>
2017-05-09 17:31:20 +0200

    test

--- www/js/login.js
+++ www/js/login.js
@@ -1,6 +1,6 @@
 Array.prototype.last = function() {return this[this.length-1];}
 
-var a="nada";
+
 
 function onDeviceReady() {
 
@@ -30,18 +30,34 @@ function iniciarFireBase(){
 
 
 function lGoogle(){
-alert("hola");
+
+
 	try{
 	var provider = new firebase.auth.GoogleAuthProvider();
-}catch(err) {
+	}catch(err) {
 	    alert(err.message);
 	}
-	alert("hola2");
 	
-		a=firebase.auth().signInWithRedirect(provider);
-	alert(a);
-	setTimeout(function(){ alert(a.ra.code); }, 1000);
-	setTimeout(function(){ alert(a.ra.message); }, 1000);
+
+
+	firebase.auth().signInWithRedirect(provider).then(function() {
+	  firebase.auth().getRedirectResult().then(function(result) {
+	    // This gives you a Google Access Token.
+	    // You can use it to access the Google API.
+	    var token = result.credential.accessToken;
+	    // The signed-in user info.
+	    var user = result.user;
+	    alert("login");
+	  }).catch(function(error) {
+	    // Handle Errors here.
+	    var errorCode = error.code;
+	    var errorMessage = error.message;
+	    alert("error");
+	  });
+	});
+
+
+
 
 }
 
